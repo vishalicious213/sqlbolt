b133a7ee1feffdb99acca8a57ee32118a9628c19
vishalicious213 <vishalicious213@gmail.com>
2024-06-04 11:21:36 -0700

    drafted and updated 03-select-queries-with-constraints.sql

---
+++ 03-select-queries-with-constraints.sql
@@ -0,0 +1,21 @@
+-- SQL Lesson 2: Queries with constraints (Pt. 2)
+
+-- 1. Find all the Toy Story movies
+
+SELECT * FROM movies
+WHERE Title LIKE "Toy Story%";
+
+-- 2. Find all the movies directed by John Lasseter
+
+SELECT * FROM movies
+WHERE Director = "John Lasseter";
+
+-- 3. Find all the movies (and director) not directed by John Lasseter
+
+SELECT * FROM movies
+WHERE Director != "John Lasseter";
+
+-- 4. Find all the WALL-* movies
+
+SELECT * FROM movies
+WHERE Title LIKE "Wall-%";
