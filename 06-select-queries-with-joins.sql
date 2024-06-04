42d7accd551b91c1041baecab4d085975136bdf1
vishalicious213 <vishalicious213@gmail.com>
2024-06-04 14:15:13 -0700

    drafted 06-select-queries-with-joins.sql

---
+++ 06-select-queries-with-joins.sql
@@ -0,0 +1,12 @@
+-- Lesson 6: Multi-table queries with JOINs
+
+-- 1. Find the domestic and international sales for each movie
+
+
+
+-- 2. Show the sales numbers for each movie that did better internationally rather than domestically
+
+
+
+-- 3. List all the movies by their ratings in descending order
+
