1305acd89cbc3c7ea5933069bceff30b0bd09faa
vishalicious213 <vishalicious213@gmail.com>
2024-06-04 17:54:26 -0700

    updated 07-select-queries-with-outer-joins.sql

--- 07-select-queries-with-outer-joins.sql
+++ 07-select-queries-with-outer-joins.sql
@@ -2,11 +2,16 @@
 
 -- 1. Find the list of all buildings that have employees ✓
 
-
+SELECT DISTINCT Building FROM employees;
 
 -- 2. Find the list of all buildings and their capacity
 
-
+SELECT * FROM buildings;
 
 -- 3. List all buildings and the distinct employee roles in each building (including empty buildings)
 
+SELECT DISTINCT building_name, role FROM buildings
+LEFT JOIN Employees
+ON building_name = building;
+
+-- DISTINCT applies to all columns in the query, it's not used for an individual column
