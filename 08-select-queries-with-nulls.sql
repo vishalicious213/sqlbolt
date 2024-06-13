aa19ff26f1bf7260f8db2ba7930e83b4530de868
vishalicious213 <vishalicious213@gmail.com>
2024-06-13 14:40:28 -0700

    drafted 08-select-queries-with-nulls.sql

---
+++ 08-select-queries-with-nulls.sql
@@ -0,0 +1,8 @@
+-- SQL Lesson 8: A short note on NULLs
+
+-- 1. Find the name and role of all employees who have not been assigned to a building
+
+
+
+-- 2. Find the names of the buildings that hold no employees
+
